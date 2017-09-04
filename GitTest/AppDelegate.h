3d2a84b7deabfab3b8d56f4ffdd2cd267d1b4825
CliffLee <Cliff.Lee@etatung.com>
2017-09-04 11:03:11 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by 李振仁 on 2017/9/4.
+//  Copyright © 2017年 李振仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by 李振仁 on 2017/9/4.
+//  Copyright © 2017年 李振仁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
